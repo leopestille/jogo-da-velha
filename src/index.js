66e47e1fa855555b39dd40f134d4e128abb3e06d
Leo Pestille <leonardopestille@gmail.com>
2022-12-28 12:20:29 -0400

    classe Game

--- src/index.js
+++ src/index.js
@@ -39,4 +39,20 @@ class Board extends React.Component {
             </div>
         )
     }
+}
+
+class Game extends React.Component {
+    render() {
+        return (
+            <div className='game'>
+                <div className='game-board'>
+                    <Board />
+                </div>
+                <div className='game-info'>
+                    <div>{/* status */}</div>
+                    <ol>{/* TODO */}</ol>
+                </div>
+            </div>
+        );
     }
+}
